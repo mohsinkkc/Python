65c20631193e28011bbd3041bfe169e05d9eb032
bhumika1121 <sarvaiya.bhumika85@gmail.com>
2023-08-05 11:50:53 +0530

    merge other

---
+++ Daily_task/assignment.py
@@ -0,0 +1,11 @@
+num= int(input("enter num :"))
+
+num1 = int(input("enter num 1:"))
+
+if num>=0 and num1>=0:
+    print("both are positive")
+else    :
+    print("both are -ve")
+
+
+
---
+++ Daily_task/first.py
@@ -0,0 +1,26 @@
+print("Hello All \nGood Afternoon")
+
+print('My Mom\'s Birthday')
+
+print("The \"Elephent\"")
+
+
+print("""=========Popular Place to visit===========
+            1. Ahmedabad
+            2. Mumbai
+            3. Kolkata
+      """)
+
+
+# end="" : end paramatere that define ending of string
+print("Hello",end="$")
+print("World")
+
+#sep="" : sep partameter will define the seperation of two or more strings
+print('\n==with sep===\n')
+print("Hola","Amigo",sep="?")
+print("Morning !")
+
+print('\n==with end===\n')
+print("Hola","Amigo",end="?")
+print("Morning !")
