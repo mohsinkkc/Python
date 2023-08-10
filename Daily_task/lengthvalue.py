513fa5f81b864321d15abb6bd60ece79668cd8e2
mohsinkkc <125197013+mohsinkkc@users.noreply.github.com>
2023-08-10 13:46:33 +0530

    Add files via upload

---
+++ Daily_task/lengthvalue.py
@@ -0,0 +1,7 @@
+value="my name is mohsin"
+
+a=0
+
+for i in value:
+    a+=1
+    print("the length of value is :",a)
---
+++ Daily_task/reversestring.py
@@ -0,0 +1,4 @@
+name=input("enter the string :")
+
+print("before revrse string :",name)
+print("after reverse string",name[-1:0:-1])
