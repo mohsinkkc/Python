afcff8ebca42923ca65169583eea7a191c30855e
mohsinkkc <125197013+mohsinkkc@users.noreply.github.com>
2023-08-22 13:40:24 +0530

    Add files via upload

---
+++ Daily_task/funct of add,mul,sub.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+a=int(input("enter the number a:"))
+b=int(input("enter the number b:"))
+
+print("addition is :",add(a,b))
+print("substraction is:",sub(a,b))
+print("multiplication is:",mul(a,b))
+print("division is:",div(a,b))
